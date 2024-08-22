bd1a1b2612dd45cf7e07691b29a230d5a502bf7b
Satria-WS <extencezone02@gmail.com>
2024-08-22 10:34:52 +0700

    update home component

--- 2.Angular/app_project/src/app/home/home.component.ts
+++ 2.Angular/app_project/src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import { RoomsComponent } from '../rooms/rooms.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RoomsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
