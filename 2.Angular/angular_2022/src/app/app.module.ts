79d2c024dc82cd5fe886e8db0dc272fa956208b3
Satria-WS <extencezone02@gmail.com>
2024-08-26 18:16:56 +0700

    adding route

---
+++ 2.Angular/angular_2022/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    // Note: No need to declare ConceptComponent and InterpolationComponent here
+  ],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [],
+})
+export class AppModule {}
--- 2.Angular/angular_2022/src/app/app.routes.ts
+++ 2.Angular/angular_2022/src/app/app.routes.ts
@@ -1,9 +1,26 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ConceptComponent } from './concept/concept.component';
+import { InterpolationComponent } from './concept/interpolation/interpolation.component';
+import { TwoWayBindingComponent } from './concept/two-way-binding/two-way-binding.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'concept',
+    component: ConceptComponent,
+    children: [
+      {
+        path: 'interpolation',
+        component: InterpolationComponent,
+      },
+      {
+        path: 'two-way-binding',
+        component: TwoWayBindingComponent,
+      }
+    ],
   },
 ];
