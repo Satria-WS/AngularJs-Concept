1d80fd231f6737383720cc5ceb81e4a55ed28321
Satria-WS <extencezone02@gmail.com>
2024-08-27 17:55:24 +0700

    APPMODULE

--- 2.Angular/angular_2022/src/app/app.module.ts
+++ 2.Angular/angular_2022/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
+
 @NgModule({
   declarations: [
     // Note: No need to declare ConceptComponent and InterpolationComponent here
--- 2.Angular/angular_2022/src/app/app.routes.ts
+++ 2.Angular/angular_2022/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { HomeComponent } from './home/home.component';
 import { ConceptComponent } from './concept/concept.component';
 import { InterpolationComponent } from './concept/interpolation/interpolation.component';
 import { TwoWayBindingComponent } from './concept/two-way-binding/two-way-binding.component';
+import { DirectiveComponent } from './concept/directive/directive.component';
+import { PipexComponent } from './concept/pipex/pipex.component';
 
 export const routes: Routes = [
   {
@@ -20,6 +22,14 @@ export const routes: Routes = [
       {
         path: 'two-way-binding',
         component: TwoWayBindingComponent,
+      },
+      {
+        path: 'directive',
+        component: DirectiveComponent,
+      },
+      {
+        path: 'pipe',
+        component: PipexComponent,
       }
     ],
   },
