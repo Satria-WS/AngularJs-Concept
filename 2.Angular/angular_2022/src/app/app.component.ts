1dffcda451542858ff175cdba96f604db6fdc3e1
Satria-WS <extencezone02@gmail.com>
2024-08-30 18:38:19 +0700

    update route

--- 2.Angular/angular_2022/src/app/app.component.ts
+++ 2.Angular/angular_2022/src/app/app.component.ts
@@ -1,15 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular_2022';
+
+
+  // ngOnInit(): void {
+  //   initializeApp(firebaseConfig);
+  // }
+
+  isAuthenticated() {
+    // return this.authService.isAuthenticated;
+    return true;
+  }
+
+  logout() {
+    // this.authService.logout();
+    return true;
+  }
 }
