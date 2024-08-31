bfdf170b6a9930433c5bbcea1563f44453b0e184
Satria-WS <extencezone02@gmail.com>
2024-08-31 13:30:05 +0700

    update book component

--- 2.Angular/angular_2022/src/app/books/books.component.ts
+++ 2.Angular/angular_2022/src/app/books/books.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookComponent } from './book/book.component';
 import { Book } from '../types/Book';
@@ -12,13 +12,29 @@ import { BooksService } from './books.service';
   templateUrl: './books.component.html',
   styleUrl: './books.component.css'
 })
-export class BooksComponent implements OnInit {
+export class BooksComponent implements OnInit , OnDestroy {
   books: Book[] = [];
-  constructor(private booksService: BooksService) { }
-
   isShowing: boolean = true;
 
+  constructor(private booksService: BooksService) {
+
+
+  }
+
+
+
   ngOnInit(): void {
+    console.log('ths on Init');
     this.books = this.booksService.getBooks();
+
+  }
+
+  ngOnDestroy(): void {
+    return;
+  }
+
+  addToCard(event: Book) {
+    // console.log(event)
+    console.log('heelo');
   }
 }
