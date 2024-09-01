763fcb813adc90ee7fc730eb9fa0878b54c986d4
Satria-WS <extencezone02@gmail.com>
2024-09-01 18:23:24 +0700

    update book`

--- 2.Angular/angular_2022/src/app/books/book/book.component.ts
+++ 2.Angular/angular_2022/src/app/books/book/book.component.ts
@@ -1,14 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Book } from '../../types/Book';
+import { CartService } from '../../cart/cart.service';
 @Component({
   selector: 'app-book',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './book.component.html',
-  styleUrl: './book.component.css'
+  styleUrl: './book.component.css',
 })
-export class BookComponent {
-  @Input() book: Book = {} as Book;
+export class BookComponent implements OnInit {
+  @Input() book: Book = {
+    name: '',
+    author: '',
+    image: '',
+    amount: 0,
+  };
+  @Output() emitBook = new EventEmitter<Book>();
   isInCart: boolean = false;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    return;
+  }
+  addToCard() {
+    this.isInCart = true;
+    this.cartService.add(this.book);
+  }
+
+  removeFromCart() {
+    this.isInCart = false;
+    this.cartService.remove(this.book);
+  }
 }
