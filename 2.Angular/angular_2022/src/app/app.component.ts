099551e1f2d44feea0642c599b6619f8a973416a
Satria-WS <extencezone02@gmail.com>
2024-09-01 18:20:48 +0700

    update compoenent

--- 2.Angular/angular_2022/src/app/app.component.ts
+++ 2.Angular/angular_2022/src/app/app.component.ts
@@ -1,21 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
+
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    FormsModule,
-    RouterModule,
-    CommonModule,
-    CartComponent,
-  ],
+  imports: [RouterOutlet, FormsModule, CommonModule, CartComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
