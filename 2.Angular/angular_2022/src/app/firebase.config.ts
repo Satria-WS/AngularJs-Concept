74c7492010b6c9302c60d7eee4b50db68090af8d
Satria-WS <extencezone02@gmail.com>
2024-09-03 16:01:33 +0700

    configuration firebase

---
+++ 2.Angular/angular_2022/src/app/firebase.config.ts
@@ -0,0 +1,11 @@
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDqyyKl9pAivLSoMrJwDRDDI4wJz3Ed9Fo',
+  authDomain: 'angular22-course-a2fe4.firebaseapp.com',
+  projectId: 'angular22-course-a2fe4',
+  storageBucket: 'angular22-course-a2fe4.appspot.com',
+  messagingSenderId: '968489306469',
+  appId: '1:968489306469:web:a049d3737df4c9f883398c',
+};
+
+// export const firebase = initializeApp(firebaseConfig);
