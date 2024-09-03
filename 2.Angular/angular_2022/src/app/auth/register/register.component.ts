eaf783dd443e791fb299fd24ca40873d86056dce
Satria-WS <extencezone02@gmail.com>
2024-09-03 16:02:34 +0700

    fixing register

--- 2.Angular/angular_2022/src/app/auth/register/register.component.ts
+++ 2.Angular/angular_2022/src/app/auth/register/register.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RegisterForm } from '../../types/Auth';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  form: RegisterForm = {
+    email: '',
+    password: '',
+    confirm_password: '',
+  };
+  passwordMatched: boolean = true;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    return;
+  }
+
+  submit() {
+
+    if (this.form.password !== this.form.confirm_password) {
+      this.passwordMatched = false;
+    }
+    // this.authService.register(this.form);
+    console.log(this.form);
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, this.form.email, this.form.password)
+      .then(userCredential => {
+        // Signed up
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const user = userCredential.user;
+        console.log(userCredential);
+        // ...
+      })
+      .catch(error => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const errorCode = error.code;
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const errorMessage = error.message;
+        // ..
+      });
+  }
+
+  isLoading() {
+    //return this.authService.isLoading;
+    return;
+  }
 }
