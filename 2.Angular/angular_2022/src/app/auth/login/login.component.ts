4c1c1ef3745302b1a07eaf11acf9b668cf600e83
Satria-WS <extencezone02@gmail.com>
2024-09-03 16:02:14 +0700

    fixing login template

--- 2.Angular/angular_2022/src/app/auth/login/login.component.ts
+++ 2.Angular/angular_2022/src/app/auth/login/login.component.ts
@@ -1,9 +1,9 @@
-
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
-
+import { LoginForm } from '../../types/Auth';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -14,7 +14,41 @@ import { FormsModule } from '@angular/forms';
 export class LoginComponent {
 
 
+  form: LoginForm = {
+    email: '',
+    password: '',
+  };
+
+  constructor(private authService: AuthService) { }
+
+
   submit() {
-    return;
+    // return;
+
+    // if (this.isLoading) return;
+    // this.isLoading = true;
+    // console.log(this.form);
+    // const auth = getAuth();
+    // signInWithEmailAndPassword(auth, this.form.email, this.form.password)
+    //   .then(userCredential => {
+    //     // Signed in
+    //     // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    //     console.log(userCredential);
+    //     alert('login succes');
+    //     // ...
+    //   })
+    //   .catch(error => {
+    //     // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    //     const errorCode = error.code;
+    //     // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    //     const errorMessage = error.message;
+    //     alert('gagal govlok');
+    //   })
+    //   .finally(() => (this.isLoading = false));
+    this.authService.login(this.form);
+  }
+
+  isLoading() {
+    return this.authService.isLoading;
   }
 }
