a5a72b99a3c7ac74e92559e955c30bb4ff729217
Satria-WS <extencezone02@gmail.com>
2024-09-04 17:33:01 +0700

    updater auth guard

--- 2.Angular/angular_2022/src/app/auth/auth.guard.ts
+++ 2.Angular/angular_2022/src/app/auth/auth.guard.ts
@@ -1,5 +1,27 @@
 import { CanActivateFn } from '@angular/router';
+import { AuthService } from './auth.service';
+import { inject } from '@angular/core';
 
-export const authGuard: CanActivateFn = (_route, state) => {
-  return true;
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  return authService.isAuthenticated;
 };
+// import { Injectable } from '@angular/core';
+// import { CanActivate } from '@angular/router';
+// import { AuthService } from './auth.service';
+
+
+
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class AuthGuard implements CanActivate {
+//   constructor(private authService: AuthService) {}
+
+//   canActivate() {
+//     return this.authService.isAuthenticated;
+//   }
+// }
+
+
